467db7a38f1b2d5d43a669fd47d733683afb4d54
AlexFilipps <AKFilipps@gmail.com>
2021-11-28 09:57:01 -0500

    fixed player and added basic keyListener

--- lameJam2021/Assets/GameLogicScripts/Player.cs
+++ lameJam2021/Assets/GameLogicScripts/Player.cs
@@ -15,26 +15,43 @@ public class Player : MonoBehaviour
 {
 
     private int direction; //represents the direction the model needs to face 0-5 being the 6 possible directions
-    curSelectedMove curSelectMove;
+    curSelectedMove curSelectMove = curSelectedMove.Nothing;
 
-    private int xPos;
-    private int yPos;
+    private int xPos = 1;
+    private int yPos = 1;
 
     public Weapon[] weapons;
 
     public GameObject playerObject;
 
+    public int[,] possibleMoves = new int[6, 2];
+
 
     // Start is called before the first frame update
     void Start()
     {
-        EventManager.current.selectTileEvent += MoveCharacter;
+        //possibleMoves = new int[6, 2];
+        EventManager.current.selectTileEvent += MakeMove;
+        EventManager.current.selectMoveEvent += setMove;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void setMove(int id)
+    {
+        if (id == 0)
+        {
+            curSelectMove = curSelectedMove.Nothing;
+        }
+        if (id == 1)
+        {
+            curSelectMove = curSelectedMove.Move;
+            setPossibleMoves();
+        }
     }
 
     public void MakeMove(int x, int y)
@@ -63,11 +80,11 @@ public class Player : MonoBehaviour
             
             for (int i = 0; i < 6; i++)
             {
-                int[] coords = CalculateMove(x, y, i);
-                if (x == coords[0] && y == coords[1])
+                if (x == possibleMoves[i,0] && y == possibleMoves[i, 1])
                 {
                     // Move is valid! move there. (Alex working on this)
                     setPosition(x, y);
+                    break;
                 }
             }
             
@@ -75,6 +92,8 @@ public class Player : MonoBehaviour
         }
         
     }
+
+    /*
     private int[] CalculateMove(int x, int y, int dirVec)
     {
         // Start with current (x,y) coordinates
@@ -83,10 +102,21 @@ public class Player : MonoBehaviour
         switch (dirVec)
         {
             case 0:
-                coords[0]--;
+                print("checking if position " + coords[0] + " " + coords[1] + "is left to the player");
+                coords[0]++;
                 break;
 
             case 1:
+                print("checking if position " + coords[0] + " " + coords[1] + "is up left to the player");
+                if (coords[1] % 2 != 0)
+                {
+                    coords[0]++;
+                }
+                coords[1]--;
+                break;
+
+            case 2:
+                print("checking if position " + coords[0] + " " + coords[1] + "is up right to the player");
                 if (coords[1] % 2 == 0)
                 {
                     coords[0]--;
@@ -94,19 +124,13 @@ public class Player : MonoBehaviour
                 coords[1]--;
                 break;
 
-            case 2:
-                if (coords[1] % 2 != 0)
-                {
-                    coords[0]--;
-                }
-                coords[1]--;
-                break;
-
             case 3:
-                coords[0]++;
+                print("checking if position " + coords[0] + " " + coords[1] + "is right to the player");
+                coords[0]--;
                 break;
 
             case 4:
+                print("checking if position " + coords[0] + " " + coords[1] + "is down right to the player");
                 if (coords[1] % 2 == 0)
                 {
                     coords[0]--;
@@ -115,6 +139,7 @@ public class Player : MonoBehaviour
                 break;
 
             case 5:
+                print("checking if position " + coords[0] + " " + coords[1] + "is down left to the player");
                 if (coords[1] % 2 != 0)
                 {
                     coords[0]++;
@@ -124,9 +149,11 @@ public class Player : MonoBehaviour
         }
         return coords;
     }
+    */
 
     public void setPosition(int x, int y)
     {
+        /*
         print("setting pos");
         //THIS IS REALLY DUMB
         float HexagonWidth = 1.75f;
@@ -134,10 +161,70 @@ public class Player : MonoBehaviour
         float newY = y * (1.5f / Mathf.Sqrt(3)) * HexagonWidth;
         if (y % 2 == 0)
         {
-            newX = (x * 1 * HexagonWidth) + (HexagonWidth / 2);
+            newX = (x * 1 * HexagonWidth) - (HexagonWidth / 2);
         }
         //MANUEL WILL HANDLE IT THO
 
         this.transform.position = new Vector3(newX, 2, newY);
+        */
+
+        Tile tile = Game.current.getTile(x, y);
+        this.transform.position = tile.tile.transform.position;
+        this.transform.position = new Vector3(this.transform.position.x, 2, this.transform.position.z);
+        xPos = x;
+        yPos = y;
+        setPossibleMoves();
+    }
+
+
+    private void setPossibleMoves()
+    { 
+        possibleMoves[0, 0] = xPos - 1;
+        possibleMoves[0, 1] = yPos;
+
+        possibleMoves[1, 1] = yPos + 1;
+
+        possibleMoves[2, 1] = yPos + 1;
+
+        possibleMoves[3, 0] = xPos + 1;
+        possibleMoves[3, 1] = yPos;
+
+        possibleMoves[4, 1] = yPos - 1;
+
+        possibleMoves[5, 1] = yPos - 1;
+
+        if (yPos % 2 == 0)
+        {
+            possibleMoves[1, 0] = xPos - 1;
+            possibleMoves[2, 0] = xPos;
+            possibleMoves[4, 0] = xPos;
+            possibleMoves[5, 0] = xPos - 1;
+        }
+        else
+        {
+            possibleMoves[1, 0] = xPos;
+            possibleMoves[2, 0] = xPos + 1;
+            possibleMoves[4, 0] = xPos + 1;
+            possibleMoves[5, 0] = xPos;
+        }
+
+        print(possibleMoves);
+        for (int i = 0; i < 6; i++)
+        {
+            print(possibleMoves[i,0] + " and " + possibleMoves[i,1]);
+        }
+    }
+
+
+    protected void rotateToCurrDirection()
+    {
+        this.transform.RotateAround
+        (
+        new Vector3(this.transform.position.x,
+                    this.transform.position.y,
+                    this.transform.position.z),
+        new Vector3(0, 1, 0),
+        (60 * (int)direction)
+        );
     }
 }
